d89849bf453d05edf7a42d4fc280daef648ce01e
s4sarath <s4sarath@gmail.com>
2020-11-17 13:49:44 +0530

    Added release draft actions

--- src/mycalc2/__init__.py
+++ src/mycalc2/__init__.py
@@ -1,1 +1,13 @@
-__version__ = "0.1.0"
+"""The hypermodern Python project."""
+try:
+    from importlib.metadata import PackageNotFoundError, version  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+# __version__ = "0.1.0"
